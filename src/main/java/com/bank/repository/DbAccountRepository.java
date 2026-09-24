9d37a4cad5e6c92cf52a52fbc170ca3984c1efee
Gustavo Nepomuceno Capistrano <gustavo.capy@gmail.com>
2026-09-24 16:47:12 -0300

    refactor: refactor save account functionality to persist on database

--- src/main/java/com/bank/repository/DbAccountRepository.java
+++ src/main/java/com/bank/repository/DbAccountRepository.java
@@ -1,7 +1,11 @@
 package main.java.com.bank.repository;
 
 import main.java.com.bank.model.Account;
+import main.java.com.bank.persistence.DatabaseConnection;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,7 +14,41 @@ public class DbAccountRepository implements AccountRepository {
 
     @Override
     public void save(Account account) {
-        accounts.add(account);
+        String sql = "INSERT INTO accounts (account_number, holder_name, balance, account_type) VALUES (?, ?, ?, ?)";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = DatabaseConnection.connectDb();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, account.getAccountNumber());
+            pstmt.setString(2, account.getHolder().getName());
+            pstmt.setDouble(3, account.getBalance());
+            pstmt.setString(4, account.getClass().getSimpleName());
+
+            pstmt.executeUpdate();
+            System.out.println("Account save success.");
+        } catch (SQLException e) {
+            System.out.println("ERROR:" + e.getMessage());
+            e.printStackTrace();
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
